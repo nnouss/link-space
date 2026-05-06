64d80a299461e1e2e753ffe41181b92116c2a975
nnouss <devnnouss@gmail.com>
2026-05-06 15:16:00 +0900

    test: Google 검색어 파서 경계 케이스 추가

--- src/shared/google.test.ts
+++ src/shared/google.test.ts
@@ -9,6 +9,13 @@ describe('parseGoogleSearch', () => {
     });
   });
 
+  it('google.com bare host search URL에서 q 값을 추출한다', () => {
+    expect(parseGoogleSearch('https://google.com/search?q=plain')).toEqual({
+      engine: 'google',
+      query: 'plain'
+    });
+  });
+
   it('google.co.kr search URL에서 q 값을 추출한다', () => {
     expect(parseGoogleSearch('https://www.google.co.kr/search?q=%EB%A7%81%ED%81%AC')).toEqual({
       engine: 'google',
@@ -22,5 +29,9 @@ describe('parseGoogleSearch', () => {
 
   it('q 값이 비어 있으면 null을 반환한다', () => {
     expect(parseGoogleSearch('https://www.google.com/search?q=')).toBeNull();
+  });
+
+  it('유효하지 않은 URL이면 null을 반환한다', () => {
+    expect(parseGoogleSearch('not a url')).toBeNull();
   });
 });
